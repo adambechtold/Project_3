9148c98c876d59faac5fca9f7991d7a00ce6ad55
adambechtold <adamcbechtold@gmail.com>
2017-03-04 21:49:55 -0500

    grid contructor made in the main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include "Dictionary.h"
+#include "d_matrix.h"
 
 using namespace std;
 
@@ -16,17 +17,49 @@ int main() {
     testDict.selectionSort();
     cout << testDict;
 
-    int a = testDict.lookup("a");
-    int b = testDict.lookup("bactericide");
-    int c = testDict.lookup("skulls");
-    int e = testDict.lookup("awarded");
-    int r = testDict.lookup("unreeves");
+//    int a = testDict.lookup("a");
+//    int b = testDict.lookup("bactericide");
+//    int c = testDict.lookup("skulls");
+//    int e = testDict.lookup("awarded");
+//    int r = testDict.lookup("unreeves");
+//
+//    cout << "a: " << a << endl;
+//    cout << "b: " << b << endl;
+//    cout << "c: " << c << endl;
+//    cout << "e: " << e << endl;
+//    cout << "r: " << r << endl;
 
-    cout << "a: " << a << endl;
-    cout << "b: " << b << endl;
-    cout << "c: " << c << endl;
-    cout << "e: " << e << endl;
-    cout << "r: " << r << endl;
+    ifstream fin;
+    string fileName = "15x15grid.txt";
+    fin.open(fileName);
+    if (!fin){
+        cout << "File access failure.\n";
+    }
+    string nRowStr;
+    string nColStr;
+
+    fin >> nRowStr;
+    fin >> nColStr;
+
+    int nRow = stoi(nRowStr);
+    int nCol = stoi(nColStr);
+
+
+    string letter;
+
+    matrix<string> testMat = matrix<string>(nRow, nCol);
+
+    for (int i = 0; i < nRow; i++) {
+        for(int j = 0; j < nCol; j++) {
+            fin >> letter;
+            testMat[i][j] = letter;
+        }
+        cout << endl;
+    }
+
+    testMat.print();
+
+
 
     return 0;
     //did this go through?
